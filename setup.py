33d211ece10f025ebc72a3618918175a5a53082b
Skye Thompson <rthompson@theaiinstitute.com>
2025-01-27 11:37:37 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='factor_world',
+    version='0.1.0',    
+    description='',
+    url='',
+    author='',
+    author_email='',
+    license='',
+    packages=['factor_world'],
+    install_requires=[],
+
+    classifiers=[
+    ],
+)
